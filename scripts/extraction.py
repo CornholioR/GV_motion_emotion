9350b5826122ece34695043929d72e3a566072b6
Draghoyns <sakula.hys@polytechnique.org>
2024-05-28 07:07:48 +0200

    relative path for data

--- scripts/extraction.py
+++ scripts/extraction.py
@@ -13,17 +13,16 @@ import pandas as pd
 
 import os
 
-directory = "C:/Users/User/Desktop/Todai classes/S1 Robotic/data/CSV"
+directory = "CSV"
 i = 0
 data_dict = {}
 for filename in os.listdir(directory):
     i += 1
     f = os.path.join(directory, filename)
     emotion = filename[-11:-8]
-    print(emotion)
 
     df = pd.read_csv(
-        "C:/Users/User/Desktop/Todai classes/S1 Robotic/data/CSV/" + filename
+        "CSV/" + filename
     )
     df = df.drop(0)
     df = df.drop(1)
@@ -32,9 +31,10 @@ for filename in os.listdir(directory):
     df = df.drop(4)
     column_indices_to_keep = [j for j in range(150)]
     column_indices_to_keep = column_indices_to_keep[2:11]
-    print(column_indices_to_keep)
     df_filtered = df.iloc[:, column_indices_to_keep]
     # df_filtered["Label"] = emotion
     data_dict[i] = df_filtered
+
+
 
 
